f4c2149b919dc7ca065a6c194e6366cd21d4e404
sushmitha107 <80115549+sushmitha107@users.noreply.github.com>
2021-05-20 21:26:32 +0530

    Update caseexamplee.sh

--- caseexamplee.sh
+++ caseexamplee.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #Take user Input
-echo "Enter two numbers : "
-read a
-read b
+echo $a $b $ch
+A=$a
+B=$b
+C=$ch
 #Input Type of Operation
 echo "Enter Choice : "
 echo "1. Addition"
@@ -12,22 +13,22 @@ echo "4. Division"
 read ch
 #switch case to perform
 #calculator operations
-case $ch  in
+case $C  in
 	1)
-	val=`expr $a + $b`  #To perform addition operation
+	val=`expr $A + $B`  #To perform addition operation
 	echo -e " a+b=\e[1;31m $val \e[0m" #Print the values in red coloured format
 
 	;;
 	2)
-	val=`expr $a - $b` #To perform subtract operation
+	val=`expr $A - $B` #To perform subtract operation
 	echo -e "a-b=\e[1;33m $val \e[0m" #Print the values in yellow coloured format
 	;;
 	3)
-	val=`expr $a \* $b` #To perform multiplication operation
+	val=`expr $A \* $B` #To perform multiplication operation
 	echo -e "a*b=\e[1;34m $val \e[0m" #Print the values in blue coloured format
 	;;
 	4)
-	val=`expr $a / $b` #To perform division operation
+	val=`expr $A / $B` #To perform division operation
 	 echo -e "a/b=\e[1;32m $val \e[0m" #Print the values in green coloured format
 	 ;;
  esac
